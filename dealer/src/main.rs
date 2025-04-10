7fa79f66c29b16019627deb784eb13cf72772500
Jose Contreras <contrerasgedler@gmail.com>
2025-04-10 07:41:17 -0400

    Implement the Dealer as a server that processes a signing request

--- dealer/src/main.rs
+++ dealer/src/main.rs
@@ -1,30 +1,49 @@
-#[warn(dead_code)]
-// dealer/src/main.rs
-use anyhow::Result;
-use dealer::Dealer;
-use rand::thread_rng;
-
 mod dealer;
+
+use crate::dealer::Dealer;
+use anyhow::{Context, Result};
+use p2p::RawProtocolMessage;
+
+use tokio::sync::mpsc;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let dealer = Dealer;
+    println!("Starting Dealer");
 
-    // Simulate receiving a message from the gateway via p2p (for testing purposes)
-    let message = b"hello from gateway";
+    let grpc_address = "127.0.0.1".to_string();
+    let grpc_port = 50051;
 
-    let mut rng = thread_rng();
-    let max_signers = 3;
-    let min_signers = 2;
-    let (shares, pubkey_package) = frost_ed25519::keys::generate_with_dealer(
-        max_signers,
-        min_signers,
-        frost_ed25519::keys::IdentifierList::Default,
-        &mut rng,
-    )
-    .expect("Failed to generate key package");
-    println!("Key package: {:?}", pubkey_package);
-    println!("Shares: {:?}", shares);
+    println!("listening on {}:{}", grpc_address, grpc_port);
+
+    // === Create communication channels ===
+    let (_, command_receiver) = mpsc::unbounded_channel();
+    let (event_sender, _) = mpsc::unbounded_channel();
+    let (message_sender, message_receiver) = mpsc::unbounded_channel();
+
+    let _node = p2p::Node::new(command_receiver, message_sender, event_sender)?;
+
+    //node.run().await?;
+
+    // === Start gRPC API ===
+    let api = Dealer::new(
+        grpc_address,
+        grpc_port,
+        None, // No timeout
+        None, // No concurrency limit
+    );
+    tokio::spawn(async move {
+        if let Err(e) = api.start().await.context("Unable to start gRPC API") {
+            println!("API start failed: {:?}", e);
+        }
+    });
+
+    handle_messages(message_receiver).await;
 
     Ok(())
+}
+
+pub async fn handle_messages(mut message_receiver: mpsc::UnboundedReceiver<RawProtocolMessage>) {
+    while let Some(msg) = message_receiver.recv().await {
+        println!("Received Message, ignored: {:?}", msg);
+    }
 }
